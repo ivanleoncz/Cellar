032a5fe3e8ce9fdd1462e4983293962eef871a1c
ivanlmj <ivanlmj@gmail.com>
2017-07-13 10:15:24 -0500

    improving app.py configuration
    - fixing debug = true variable
    - adding app.run() statement

--- render_html_template/app.py
+++ render_html_template/app.py
@@ -4,9 +4,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config(Debug=true)
+app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
-    message="Template was loaded"
-    return render_template('index.html',msg=message)
+    message="Jinja2 template message loaded via..."
+    return render_template('index.html',title=message)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8000)
