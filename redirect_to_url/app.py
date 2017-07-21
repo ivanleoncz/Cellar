710231ddc3fe9578bdd4b5b81c3fc0dd06b82314
ivanlmj <ivanlmj@gmail.com>
2017-07-21 17:10:42 -0500

    renaming example/template + debug option update

--- redirect_to_url/app.py
+++ redirect_to_url/app.py
@@ -4,8 +4,6 @@ from flask import Flask, redirect
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
-
 @app.route("/")
 def index():
     return redirect('/you_were_redirected')
@@ -15,4 +13,4 @@ def redirected():
     return "You were redirected. Congrats :)!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8000)
+    app.run(host="0.0.0.0",port=8000,debug=True)
