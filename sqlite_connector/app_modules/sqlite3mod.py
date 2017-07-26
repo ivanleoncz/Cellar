a46dbf9d0b1ede6f6cd5d6bc6d0bab8ee32cf984
ivanlmj <ivanlmj@gmail.com>
2017-07-25 23:35:30 -0500

    fixing sqlite3mod.py for "sqlite connector"
    - fixing class variable declaration for class constructor
    - fixing query statements
    - fixing query commit by configuring autocommit mode (isolation_level=None)

--- sqlite_connector/app_modules/sqlite3mod.py
+++ sqlite_connector/app_modules/sqlite3mod.py
@@ -10,26 +10,25 @@ class Lite:
     def __init__(self,db):
         self.db = db
 
-    def data_builder():
+    def data_builder(self):
         print("Building data...")
-        conn = lite.connect(db)
+        conn = lite.connect(self.db, isolation_level=None)
         cur = conn.cursor()
-        cur.execute('DROP TABLE IF EXISTS program_languages_ranking_ieee_2017')
-        cur.execute('CREATE TABLE program_languages_ranking_ieee_2017
-                    (Pos INT, Name TEXT, Web BOOLEAN, Mobile BOOLEAN, Desktop BOOLEAN, MicroCircuits BOOLEAN)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(1,'Python',1,0,1,0)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(2,'C',0,1,1,1)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(3,'Java',1,1,1,0)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(4,'C++',0,1,1,1)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(5,'C sharp',1,1,1,0)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(6,'R lang',0,0,1,0)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(7,'JavaScript',1,1,0,0)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(8,'PHP',1,0,0,0)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(9,'Go',1,0,1,0)')
-        cur.execute('INSERT INTO program_languages_ranking_ieee_2017 VALUES(10,'Swift',0,1,1,0)')
+        cur.execute("DROP TABLE IF EXISTS program_languages_ranking_ieee_2017")
+        cur.execute("CREATE TABLE program_languages_ranking_ieee_2017 \
+                    (Pos INT, Name TEXT, Web BOOLEAN, Mobile BOOLEAN, Desktop BOOLEAN, MicroCircuits BOOLEAN)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(1,'Python',1,0,1,0)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(2,'C',0,1,1,1)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(3,'Java',1,1,1,0)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(4,'C++',0,1,1,1)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(5,'C sharp',1,1,1,0)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(6,'R lang',0,0,1,0)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(7,'JavaScript',1,1,0,0)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(8,'PHP',1,0,0,0)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(9,'Go',1,0,1,0)")
+        cur.execute("INSERT INTO program_languages_ranking_ieee_2017 VALUES(10,'Swift',0,1,1,0)")
         conn.close()
         return "Done."
-
 
     def get_version():
         try:
