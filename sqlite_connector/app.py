35c7bda5f962c96823c1828ed91c60886b55582e
ivanlmj <ivanlmj@gmail.com>
2017-08-01 11:07:55 -0500

    improving sqlite_connectore example/template
    - adding routes (/,/index, /version)
    - changing db file extension

--- sqlite_connector/app.py
+++ sqlite_connector/app.py
@@ -2,10 +2,15 @@
 
 import json
 from app_modules import sqlite3mod
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 lite_obj = None
+
+@app.route("/")
+@app.route("/index")
+def f_index():
+    return redirect('/version')
 
 
 @app.route("/ranking")
@@ -13,9 +18,14 @@ def f_ranking():
     db_querier = lite_obj.data_querier()
     return render_template('ranking.html',ranking=db_querier)
 
+@app.route("/version")
+def f_sqlite_version():
+    db_version = lite_obj.get_version()
+    return "<h2 style='position: absolute; top: 30px; left: 50px; text-align: center'> SQLite Version: %s </h2>" % db_version
+
 
 if __name__ == "__main__":
-    lite_obj = sqlite3mod.Lite("test.db")
+    lite_obj = sqlite3mod.Lite("test.sqlite3")
     db_builder = lite_obj.data_builder()
     app.run(host="127.0.0.1",port=8000,debug=True)
 
