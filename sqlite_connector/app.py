46c6c91e794cc8b6b20c645e53c851214211bf96
ivanlmj <ivanlmj@gmail.com>
2017-08-01 01:41:45 -0500

    finalizing sqlite_connector code struture
    - importing render_template module from "flask" package
    - adding data_builder and data_querier functions from "Lite" class
    - adding app.run instruction for Flask

--- sqlite_connector/app.py
+++ sqlite_connector/app.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
+import json
 from app_modules import sqlite3mod
+from flask import Flask, render_template
+
+app = Flask(__name__)
+lite_obj = None
 
 
-lite_obj = sqlite3mod.Lite("test.db")
+@app.route("/ranking")
+def f_ranking():
+    db_querier = lite_obj.data_querier()
+    return render_template('ranking.html',ranking=db_querier)
 
-db_builder = lite_obj.data_builder()
 
-print(db_builder)
+if __name__ == "__main__":
+    lite_obj = sqlite3mod.Lite("test.db")
+    db_builder = lite_obj.data_builder()
+    app.run(host="127.0.0.1",port=8000,debug=True)
+
