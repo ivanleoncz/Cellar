f31fd2174a707434cdeb24c0f2e55d35d119a923
ivanlmj <ivanlmj@gmail.com>
2017-08-08 00:35:06 -0500

    removing debug flag + specifying host

--- log_to_file/app.py
+++ log_to_file/app.py
@@ -5,12 +5,10 @@ import logging
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = False
-
 @app.route("/")
 def index():
     return "<h3> Flask is working! </h3>"
 
 if __name__ == "__main__":
     logging.basicConfig(filename='app.log',format='%(message)s',level=logging.DEBUG)
-    app.run(host="0.0.0.0",port=8000)
+    app.run(host="127.0.0.1",port=8000)
