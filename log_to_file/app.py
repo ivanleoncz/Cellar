58c547a17b095a285653204a03944cfab72f71be
ivanlmj <ivanlmj@gmail.com>
2017-08-09 01:59:37 -0500

    simple adjustment and fixies on "log_to_file"

--- log_to_file/app.py
+++ log_to_file/app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from flask import Flask, redirect
 import logging
@@ -6,9 +6,13 @@ import logging
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def f_index():
     return "<h3> Flask is working! </h3>"
 
+@app.route("/data")
+def f_data():
+    return non_existent_variable
+
 if __name__ == "__main__":
-    logging.basicConfig(filename='app.log',format='%(message)s',level=logging.DEBUG)
+    logging.basicConfig(filename='app.log',format='%(message)s',level=logging.INFO)
     app.run(host="127.0.0.1",port=8000)
