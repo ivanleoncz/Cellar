911dfc2c5668465046bfab0a4d8cd18d3c7932e9
ivanlmj <ivanlmj@gmail.com>
2017-08-13 19:36:03 -0500

    small changes for render_html_template
    - adding docstring
    - realocating Debug option

--- render_html_template/app.py
+++ render_html_template/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+"""
+This template/example demonstrates how to load Jinja2 templates (HTML) with variables.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-app.config['DEBUG'] = True
 
 @app.route("/")
 def index():
@@ -12,4 +14,4 @@ def index():
     return render_template('index.html',title=message)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8000)
+    app.run(host='127.0.0.1',port=8000,debug=True)
