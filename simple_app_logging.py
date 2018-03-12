73799d232d0c9b909400d873b912046045ad8c3c
ivanlmj <ivanlmj@gmail.com>
2018-03-11 19:04:19 -0600

    small fixies
    - improving comments and docstrings
    - adjusting getLogger option (removing single quotes from __name__)
    - removing unnecessary comments

--- simple_app_logging.py
+++ simple_app_logging.py
@@ -16,13 +16,13 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def get_index():
-    """ Route for / and /index. """
+    """ Function for / and /index routes. """
     return "Welcome to Flask! "
 
 
 @app.route("/data")
 def get_data():
-    """ Route for /data. """
+    """ Function for /data route. """
     data = {
             "Name":"Ivan Leon",
             "Occupation":"Software Developer",
@@ -34,7 +34,7 @@ def get_data():
 @app.route("/error")
 def get_nothing():
     """ Route for intentional error. """
-    return non_real_variable # intentional non existent variable
+    return non_real_variable # intentional non-existent variable
 
 
 @app.after_request
@@ -71,31 +71,9 @@ def exceptions(e):
 if __name__ == '__main__':
     # maxBytes to small number, in order to demonstrate the generation of multiple log files (backupCount).
     handler = RotatingFileHandler('app.log', maxBytes=10000, backupCount=3)
-    # getLogger('__name__'): decorators loggers to file + werkzeug loggers to stdout
-    # getLogger('werkzeug'): werkzeug loggers to file + nothing to stdout
-    logger = logging.getLogger('__name__')
+    # getLogger(__name__):   decorators loggers to file + werkzeug loggers to stdout
+    # getLogger('werkzeug'): decorators loggers to file + nothing to stdout
+    logger = logging.getLogger(__name__)
     logger.setLevel(logging.ERROR)
     logger.addHandler(handler)
     app.run(host="127.0.0.1",port=8000)
-
-
-
-### log ###
-#
-#    [2017-Aug-09 01:51] 127.0.0.1 GET http /? 200 OK
-#    [2017-Aug-09 01:51] 127.0.0.1 GET http /data? 200 OK
-#    [2017-Aug-09 01:51] 127.0.0.1 GET http /error? 5xx INTERNAL SERVER ERROR
-#    Traceback (most recent call last):
-#    File "/home/ivanlmj/git/env_flask_templates/lib/python3.4/site-packages/flask/app.py", 
-#        line 1612, in full_dispatch_request
-#    rv = self.dispatch_request()
-#    File "/home/ivanlmj/git/env_flask_templates/lib/python3.4/site-packages/flask/app.py", 
-#        line 1598, in dispatch_request
-#    return self.view_functions[rule.endpoint](**req.view_args)
-#    File "test.py", line 26, in get_json
-#    return non_real_variable # <--------------------------------------------- intentional
-#    NameError: name 'non_existent_variable' is not defined
-#
-###########
-
-# Here: https://stackoverflow.com/questions/14037975/how-do-i-write-flasks-excellent-debug-log-message-to-a-file-in-production/39284642#39284642
