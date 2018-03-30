263d1c41c1760f4b45da4d8df6003c1b9199d188
ivanlmj <ivanlmj@gmail.com>
2018-03-30 11:06:22 -0600

    adding docstrings + index route

--- cookies/app.py
+++ cookies/app.py
@@ -1,25 +1,37 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-""" Demonstrating cookie management (read/write).  """
+""" Demonstration of cookie management.  """
 
 from flask import Flask, make_response, request
 
+__author__ = "@ivanleoncz"
+
 app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def f_index():
+    return "Welcome to Flask!"
+
+
+@app.route('/write')
+def f_write():
+    """ Builds a response with a configured cookie. """
+    username = "ivanleoncz"
+    resp = make_response("<h1> Storing Cookie </h1>")
+    resp.set_cookie('username',username)
+    return resp
+
 
 @app.route('/read')
 def f_read():
+    """ Searches for a cookie """
     username = request.cookies.get('username')
     if username:
         return "Cookie Present for %s!" % username
     else:
         return "Cookie Not Present..."
 
-@app.route('/write')
-def f_write():
-    username = "Ivan Leon"
-    resp = make_response("<h1> Storing Cookie </h1>")
-    resp.set_cookie('username',username)
-    return resp
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=8000,debug=True)
