16408ea2360c6b226d738a9313730be95f8ff809
ivanlmj <ivanlmj@gmail.com>
2018-04-01 07:51:55 -0500

    improving module structure
    - adding dunder variable (__author__)
    - structuring code inside a function
    - resuming if os.path.* to a single line
    - improving messages (return)

--- sqlite_connector/sqlite3_head.py
+++ sqlite_connector/sqlite3_head.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """ Verifies the existence of a SQLite3 database file. """
 
+__author__ = "@ivanleoncz"
+
 import os
 import sys
 
-db_filename = input("Database filename (end with .sqlite3), please:")
 
-if os.path.isfile(db_filename):
-    if os.path.getsize(db_filename) > 100:
+def sqlite3_check():
+    db_filename = input("Database filename (end with .sqlite3):")
+    if os.path.isfile(db_filename) and os.path.getsize(db_filename) > 100:
         with open(db_filename,'r', encoding = "ISO-8859-1") as f:
             header = f.read(100)
             if header.startswith('SQLite format 3'):
-                print("SQLite3 database has been detected.")
+                return "INFO: database file has been detected."
+            else:
+                return "ERROR: NO database file has been found."
+
+
+print(sqlite3_check())
