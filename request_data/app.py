1cff5447b5a360b31b7220f489cc70b30f8b2e9c
ivanlmj <ivanlmj@gmail.com>
2018-04-01 08:27:04 -0500

    improving output of request headers
    - adjusting shebang headers
    - adding jsonify import
    - adding dunder variable
    - adding docstrings
    - serializing request.headers output for json (list)

--- request_data/app.py
+++ request_data/app.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+""" Demonstration of request headers returned from a request.  """
 
-from flask import Flask, request
+from flask import Flask, jsonify, request
+
+__author__ = "@ivanleoncz"
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def f_index():
-    return str(request.headers)
+    """ Transforms all request headers into a list for JSON dump. """
+    data = []
+    for line in request.headers:
+        data.append(line)
+    return jsonify(data)
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1",port=8000,debug=True)
