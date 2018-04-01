17202cac79b838042219189b7ab11193a21ac1a3
ivanlmj <ivanlmj@gmail.com>
2018-04-01 10:19:56 -0500

    removing unnecessary view/route + improvements
    - adjusting module docstring definition
    - adding view/route for ('/')

--- get_post_query_strings.py
+++ get_post_query_strings.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" Demonstration of GET/POST requests, including Query Strings. 
+""" Demonstration of GET/POST requests. 
 
 For performing tests, some CURl examples:
 
 $ curl http://127.0.0.1:8000/index
-$ curl "http://127.0.0.1:8000/qs?name=Nemo&country=Norway"
 $ curl http://127.0.0.1:8000/post -d "name=Nemo&country=Norway"
 """
 
@@ -12,28 +11,14 @@ from flask import abort, Flask, jsonify, request
 
 __author__ = "@ivanleoncz"
 
-
 app = Flask(__name__)
 
+
+@app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def f_index():
     if request.method == 'GET':
         return "Welcome to Flask!"
-    else:
-        abort(405) # aborting: Method Not Allowed
-
-
-@app.route('/qs', methods=['GET'])
-def f_data():
-    if request.method == 'GET':
-        if len(request.args) == 0:
-            return "Query string Not Found! "
-        else:
-            name    = request.args.get('name')
-            country = request.args.get('country')
-            ipaddress = request.remote_addr 
-            data = ["Query String", name, country, ipaddress]
-            return jsonify(data)
     else:
         abort(405) # aborting: Method Not Allowed
 
