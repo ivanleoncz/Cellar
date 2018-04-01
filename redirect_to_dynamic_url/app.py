17df6c0c86478e028dce10964d9067f56161ef91
ivanlmj <ivanlmj@gmail.com>
2018-04-01 08:37:12 -0500

    improving module structure
    - aidjusting shebang headers
    - adding dunder variable
    - adding docstrings
    - adjusting order of available routes/views
    - adding spaces after commas

--- redirect_to_dynamic_url/app.py
+++ redirect_to_dynamic_url/app.py
@@ -1,29 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
+""" Demonstration of redirection, to dynamic URI. """
 
 from flask import Flask, redirect, request, url_for
+
+__author__ = "@ivanleoncz"
 
 app = Flask(__name__)
 
 
+@app.route('/user/<name>')
+def hello_user(name):
+    print("Requested URI: ", request.path)
+    if name == 'admin':
+        return redirect('/admin')
+    else:
+        return redirect(url_for('hello_guest', guest=name))
+
+
 @app.route('/admin')
 def hello_admin():
-    print("\nRequested URL Redirect: ",request.path)
+    print("Requested URI (redirect): ", request.path)
     return "Hello, Admin!"
+
 
 @app.route('/guest/<guest>')
 def hello_guest(guest):
-    print("\nRequested via Redirect: ",request.path)
+    print("Requested URI (redirect): ", request.path)
     return "You are a Guest, %s!" % guest
 
 
-@app.route('/user/<name>')
-def hello_user(name):
-    print("\nRequested via Browser: ",request.path)
-    if name =='admin':
-        return redirect('/admin')
-    else:
-        return redirect(url_for('hello_guest',guest = name))
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8000,debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
