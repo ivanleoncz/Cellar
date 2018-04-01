be30bfb9d3eacb2df31bc6a3fc8521d833b5b3b7
ivanlmj <ivanlmj@gmail.com>
2018-03-31 19:11:39 -0600

    separating db filename into a variable (input())

--- sqlite_connector/sqlite3_head.py
+++ sqlite_connector/sqlite3_head.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+""" Verifies the existence of a SQLite3 database file. """
 
 import os
 import sys
 
-if os.path.isfile('test.sqlite3'):
-    if os.path.getsize('test.sqlite3') > 100:
-        with open('test.sqlite3','r', encoding = "ISO-8859-1") as f:
+db_filename = input("Database filename (end with .sqlite3), please:")
+
+if os.path.isfile(db_filename):
+    if os.path.getsize(db_filename) > 100:
+        with open(db_filename,'r', encoding = "ISO-8859-1") as f:
             header = f.read(100)
             if header.startswith('SQLite format 3'):
                 print("SQLite3 database has been detected.")
