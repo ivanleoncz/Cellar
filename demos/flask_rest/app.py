61e8186c78cfe810b74976b668c0fd4536984f27
ivanlmj <ivanlmj@gmail.com>
2018-06-01 21:01:33 -0500

    implementing resource for displaying specific task

--- demos/flask_rest/app.py
+++ demos/flask_rest/app.py
@@ -24,5 +24,13 @@ def get_tasks_v1():
     return jsonify({'tasks': tasks})
 
 
+@app.route('/todo/api/v2.0/tasks/<int:task_id>', methods=['GET'])               
+def get_tasks_v2(task_id):                                                      
+    task = [task for task in tasks if task['id'] == task_id]                    
+    if len(task) == 0:                                                          
+        abort(404)                                                              
+    return jsonify({'task': task[0]})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
