0045573d624919ba62c35ee210d66933b0779672
ivanlmj <ivanlmj@gmail.com>
2018-06-01 21:28:31 -0500

    returning JSON for Not Found task

--- demos/flask_rest/app.py
+++ demos/flask_rest/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, jsonify
+from flask import Flask, abort, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -30,6 +30,11 @@ def get_tasks_v2(task_id):
     if len(task) == 0:                                                          
         abort(404)                                                              
     return jsonify({'task': task[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error':'Not Found'}), 404)
 
 
 if __name__ == '__main__':
