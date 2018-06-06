fc0ad71eb3fce536193502cb41e1fc478da18dc8
ivanlmj <ivanlmj@gmail.com>
2018-06-06 15:02:08 -0500

    improving example

--- examples/get_post.py
+++ examples/get_post.py
@@ -4,7 +4,8 @@
 For performing tests, some CURl examples:
 
 $ curl http://127.0.0.1:8000/index
-$ curl http://127.0.0.1:8000/post -d "name=Nemo&country=Norway"
+$ curl http://127.0.0.1:8000/data -d "name=Nemo&country=Norway"
+$ curl http://127.0.0.1:8000/all_data -d "name=Nemo&country=Norway&role=Dev"
 """
 
 from flask import abort, Flask, jsonify, request
@@ -14,16 +15,19 @@ __author__ = "@ivanleoncz"
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-@app.route('/index', methods=['GET'])
-def f_index():
-    if request.method == 'GET':
-        return "Welcome to Flask!"
+@app.route('/all_data', methods=['POST'])
+def f_data():
+    if request.method == "POST":
+        print(request.headers)
+        fields = [k for k in request.form]
+        values = [request.form[k] for k in request.form]
+        data = dict(zip(fields, values))
+        return jsonify(data)
     else:
         abort(405) # aborting: Method Not Allowed
 
 
-@app.route('/post', methods=['POST'])
+@app.route('/data', methods=['POST'])
 def f_report():
     if request.method == 'POST':
         name      = request.form["name"]
