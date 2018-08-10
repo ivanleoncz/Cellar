0c8778a13acf7bf459111e486cac8f1aa8950434
ivanlmj <ivanlmj@gmail.com>
2018-08-09 21:51:39 -0500

    improving comments and dosctring

--- examples/request_headers.py
+++ examples/request_headers.py
@@ -5,17 +5,30 @@ from flask import Flask, jsonify, request
 
 __author__ = "@ivanleoncz"
 
-
 app = Flask(__name__)
 
-@app.route("/")
-def f_index():
-    """ Transforms all request headers into a list for JSON dump. """
+@app.route("/headers")
+def f_headers():
+    """ Transforms all request headers into a list for JSON document. """
     data = []
+    # request.headers returns a string which in the following lines
+    # is processed and stored as list, for exposure as JSON document.
+    # This data is public and it's related to the request only, without
+    # exposing any other data from the CGI environment that supports Flask.
     for line in request.headers:
         data.append(line)
     return jsonify(data)
 
+@app.route("/environ")
+def f_environ():
+    # Without reprocessing ALL VALUES from request.environ dict,
+    # it's not possible to deliver it as JSON document:
+    # some values (not keys) can't be processed by jsonify(), json.dumps().
+    # The data exposed here, must no be exposed to a public via requests,
+    # since that is sensitive data about the CGI environment that is
+    # supporting Flask.
+    return str(request.environ)
+
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1",port=8000,debug=True)
+    app.run(host="0.0.0.0", debug=True)
