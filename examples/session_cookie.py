682ec26d4e80c0be8437789e5893c6fa4a0d5b1e
ivanlmj <ivanlmj@gmail.com>
2018-10-22 20:14:41 -0500

    adding html form for /login + small fixies

--- examples/session_cookie.py
+++ examples/session_cookie.py
@@ -1,26 +1,38 @@
 
 import os
 import random
-from flask import Flask, request, session, abort
+from flask import Flask, request, session
 
 app = Flask(__name__)
 
 credentials = {"UserName":"ivanleoncz", "PassWord":"p455w0rd"}
+
 messages = [
             "Get some sleep.",
             "Cookies are awesome.",
-            "Have a cup of coffee."
+            "Have a cup of coffee.",
+            "Take an umbrella with you.",
+            "Do exercises."
            ]
 
-@app.route("/login", methods = ['POST'])
+@app.route("/login", methods = ['GET', 'POST'])
 def f_login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    if username == credentials.get("UserName"):
-        if password == credentials.get("PassWord"):
-            session["IsLogged"] = True
-            return "Login -> OK"
-    return "NOK"
+    login_form = " \
+      <form action='/login' method='post'> \
+        <input type='text' name='username' placeholder='Username'></input><br> \
+        <input type='password' name='password' placeholder='Password'></input><br> \
+        <input type='submit' value='Log In'></input> \
+      </form> "
+    if request.method == 'GET':
+        return login_form
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if username == credentials.get("UserName"):
+            if password == credentials.get("PassWord"):
+                session["IsLogged"] = True
+                return "Login -> OK"
+        return "NOK"
 
 
 @app.route("/message")
@@ -30,7 +42,7 @@ def f_message():
     return "You must log in."
 
 
-@app.route("/logout", methods = ['POST'])
+@app.route("/logout", methods = ['GET'])
 def f_logout():
     session.pop("IsLogged", None)
     return "Logout -> OK"
